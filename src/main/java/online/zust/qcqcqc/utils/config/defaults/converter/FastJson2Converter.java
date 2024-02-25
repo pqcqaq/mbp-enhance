cc05cb702cf086b43fc480d37d1f51980edd9ccf
pqcqaq <905739777@qq.com>
2024-02-25 21:21:20 +0800

    添加配置信息，更新版本

--- src/main/java/online/zust/qcqcqc/utils/config/defaults/converter/FastJson2Converter.java
+++ src/main/java/online/zust/qcqcqc/utils/config/defaults/converter/FastJson2Converter.java
@@ -1,9 +1,13 @@
 package online.zust.qcqcqc.utils.config.defaults.converter;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import online.zust.qcqcqc.utils.config.JsonConverter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author qcqcqc
@@ -11,6 +15,20 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnProperty(prefix = "converter", name = "type", havingValue = "fastjson2")
 public class FastJson2Converter implements JsonConverter {
+    static {
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConfig.setWriterFeatures(
+                JSONWriter.Feature.WriteNullStringAsEmpty,
+                JSONWriter.Feature.WriteLongAsString,
+                JSONWriter.Feature.WriteMapNullValue,
+                JSONWriter.Feature.WriteNullListAsEmpty,
+                JSONWriter.Feature.WriteBigDecimalAsPlain,
+                JSONWriter.Feature.IgnoreErrorGetter
+        );
+        fastJsonConfig.setCharset(StandardCharsets.UTF_8);
+    }
+
     @Override
     public <M, T> T convertValue(M entity, Class<T> clazz) {
         return JSON.parseObject(JSON.toJSONString(entity), clazz);
