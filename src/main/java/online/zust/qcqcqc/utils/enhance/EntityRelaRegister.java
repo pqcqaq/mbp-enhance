17fc7f5de38f7a767d3f3ece728f003589c7c1e1
pqcqaq <905739777@qq.com>
2024-03-28 23:43:30 +0800

    待完成，管理entity

--- src/main/java/online/zust/qcqcqc/utils/enhance/EntityRelaRegister.java
+++ src/main/java/online/zust/qcqcqc/utils/enhance/EntityRelaRegister.java
@@ -4,6 +4,7 @@ package online.zust.qcqcqc.utils.enhance;
  * @author qcqcqc
  * Date: 2024/3/28
  * Time: 23:24
+ * 实体类关系注册器，由此将entity关联注册到关联表中
  */
 public class EntityRelaRegister {
 }
--- src/main/java/online/zust/qcqcqc/utils/enhance/EntityRelation.java
+++ src/main/java/online/zust/qcqcqc/utils/enhance/EntityRelation.java
@@ -4,6 +4,7 @@ package online.zust.qcqcqc.utils.enhance;
  * @author qcqcqc
  * Date: 2024/3/28
  * Time: 23:25
+ * 通过map和树状结构来存储实体关系，便于查询，在service层初始化时注册实体关系。
  */
 public class EntityRelation {
 }
