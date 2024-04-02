a25c80ef4ea81d17429cd36afb3cb79e512aa9c2
pqcqaq <905739777@qq.com>
2024-04-02 23:48:39 +0800

    otm和mtm正向删除检查

--- src/main/java/online/zust/qcqcqc/utils/enhance/checker/CheckHandler.java
+++ src/main/java/online/zust/qcqcqc/utils/enhance/checker/CheckHandler.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import online.zust.qcqcqc.utils.EnhanceService;
 import online.zust.qcqcqc.utils.annotation.MsgOnInversePointer;
-import online.zust.qcqcqc.utils.annotation.MtMDeepSearch;
-import online.zust.qcqcqc.utils.annotation.OtMDeepSearch;
 import online.zust.qcqcqc.utils.annotation.OtODeepSearch;
 import online.zust.qcqcqc.utils.enhance.EntityInfo;
 import online.zust.qcqcqc.utils.enhance.EntityRelation;
@@ -13,7 +11,6 @@ import online.zust.qcqcqc.utils.exception.DependencyCheckException;
 import online.zust.qcqcqc.utils.utils.FieldNameConvertUtils;
 
 import java.io.Serializable;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +21,7 @@ import java.util.Map;
  * Time: 23:20
  */
 public class CheckHandler {
-    public static <T> void doCheck(T service, Serializable id) throws DependencyCheckException {
+    public static void doCheck(EnhanceService service, Serializable id) throws DependencyCheckException {
         EntityInfo<?, ? extends EnhanceService<?, ?>, ? extends BaseMapper<?>> entityInfo = EntityRelation.entityInfoMap.get(service.getClass());
         checkPrevious(id, entityInfo);
         checkNext(id, entityInfo);
@@ -34,9 +31,11 @@ public class CheckHandler {
         if (entityInfo == null) {
             throw new DependencyCheckException("未找到对应的实体类关系信息");
         }
+        // 进行一次查询，下面判断字段是否有数据
+        Object byId = entityInfo.getService().getById(id, 1);
         // 只有otm和mtm是需要反查的，然后进行一次查询，如果还有子数据，就抛出异常
-        entityInfo.getOtmNextFieldMap().forEach((entityInfo1, fields) -> fields.forEach(field -> handleOtMDeepSearch(entityInfo1.getEntityClass(), field, entityInfo1.getService(), id)));
-        entityInfo.getMtmNextFieldMap().forEach((entityInfo1, fields) -> fields.forEach(field -> handleMtMDeepSearch(field, id)));
+        entityInfo.getOtmNextFieldMap().forEach((entityInfo1, fields) -> fields.forEach(field -> handleOtMDeepSearch(byId, field)));
+        entityInfo.getMtmNextFieldMap().forEach((entityInfo1, fields) -> fields.forEach(field -> handleMtMDeepSearch(byId, field)));
     }
 
     private static void checkPrevious(Serializable id, EntityInfo<?, ? extends EnhanceService<?, ?>, ? extends BaseMapper<?>> entityInfo) {
@@ -50,15 +49,36 @@ public class CheckHandler {
         }));
     }
 
-    private static void handleMtMDeepSearch(Field declaredField, Serializable id) {
-        // MtM就是进行一次查询，如果还有子数据，就抛出异常
-        MtMDeepSearch annotation = declaredField.getAnnotation(MtMDeepSearch.class);
-
+    private static void handleMtMDeepSearch(Object entity, Field declaredField) {
+        try {
+            declaredField.setAccessible(true);
+            Object o = declaredField.get(entity);
+            if (o != null) {
+                if (o instanceof List list && !list.isEmpty()) {
+                    MsgOnInversePointer annotation = declaredField.getAnnotation(MsgOnInversePointer.class);
+                    String msg = annotation == null ? "待删除的对象的子查询存在数据：" + "(list: " + list + ")" + "无法删除" : annotation.value();
+                    throw new DependencyCheckException(msg);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            throw new DependencyCheckException("反射获取字段值失败: " + e.getMessage());
+        }
     }
 
-    private static void handleOtMDeepSearch(Class<?> entityClass, Field declaredField, EnhanceService<? extends BaseMapper<?>, ?> service1, Serializable id) {
-        OtMDeepSearch annotation = declaredField.getAnnotation(OtMDeepSearch.class);
-
+    private static void handleOtMDeepSearch(Object entity, Field declaredField) {
+        try {
+            declaredField.setAccessible(true);
+            Object o = declaredField.get(entity);
+            if (o != null) {
+                if (o instanceof List list && !list.isEmpty()) {
+                    MsgOnInversePointer annotation = declaredField.getAnnotation(MsgOnInversePointer.class);
+                    String msg = annotation == null ? "待删除的对象的子查询存在数据：" + "(list: " + list + ")" + "无法删除" : annotation.value();
+                    throw new DependencyCheckException(msg);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            throw new DependencyCheckException("反射获取字段值失败: " + e.getMessage());
+        }
     }
 
     private static void handleOtODeepSearch(Class<?> entityClass, Field declaredField, EnhanceService<? extends BaseMapper<?>, ?> service1, Serializable id) {
@@ -76,22 +96,5 @@ public class CheckHandler {
             String msg = annotation1 == null ? "待删除的对象在类" + entityClass.getSimpleName() + "(list: " + list + ")" + "中存在依赖关系，无法删除" : annotation1.value();
             throw new DependencyCheckException(msg);
         }
-    }
-
-    private static Annotation matchFieldTypeAndContainAnnotation(Field declaredFields, Class<?> entityClass) {
-        Class<?> type = declaredFields.getType();
-        if (!type.equals(entityClass)) {
-            return null;
-        }
-        if (declaredFields.isAnnotationPresent(OtODeepSearch.class)) {
-            return declaredFields.getAnnotation(OtODeepSearch.class);
-        }
-        if (declaredFields.isAnnotationPresent(OtMDeepSearch.class)) {
-            return declaredFields.getAnnotation(OtMDeepSearch.class);
-        }
-        if (declaredFields.isAnnotationPresent(MtMDeepSearch.class)) {
-            return declaredFields.getAnnotation(MtMDeepSearch.class);
-        }
-        return null;
     }
 }
